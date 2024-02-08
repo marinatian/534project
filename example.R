302be4ac22b9db1aec28a509c61fe7c28304c3ad
Hanwen <wzha258@aucklanduni.ac.nz>
2024-02-08 12:50:27 -0800

    dailylog

--- example.R
+++ example.R
@@ -1,9 +1,16 @@
 api_key <- 'f4ae84f3cc4c51353a1ae2279dd7a60b'
 
 # Example for London, UK
-current_weather <- get_current_weather(51.5074, -0.1278, api_key)
-forecast_weather <- get_forecast_weather(51.5074, -0.1278, api_key)
+#current_weather <- get_current_weather(51.5074, -0.1278, api_key)
+#forecast_weather <- get_forecast_weather(51.5074, -0.1278, api_key)
 
+# Vancouver
+current_weather <- get_current_weather(49.2497, -123.1193, api_key)
+forecast_weather <- get_forecast_weather(49.2497, -123.1193, api_key)
+
+# Toronto
+current_weather <- get_current_weather(43.7001 , -79.4163, api_key)
+forecast_weather <- get_forecast_weather(43.7001 , -79.4163, api_key)
 
 current_weather_processed <- process_current_weather(current_weather)
 forecast_weather_processed <- process_forecast_weather(forecast_weather)
@@ -25,3 +32,4 @@ visualize_current_weather(current_weather_processed)
 
 # Write the JSON data to a file
 write(json_weather_data, file = "weather_data.json")
+
